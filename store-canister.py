347dbfb4a69e1b5e1e5ec17b2ad5f9fb60d4dcbc
Victor Porton <porton.victor@gmail.com>
2024-04-03 23:13:33 +0300

    missing file

---
+++ store-canister.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+from collections import OrderedDict
+import shlex
+
+envre = re.compile(r'''^([^=]+)\s+?=\s+?(?:[\s"']*)(.+?)(?:[\s"']*)$''')
+result = OrderedDict()
+try:
+    with open(".env") as ins:
+        for line in ins:
+            match = envre.match(line)
+            if match is not None:
+                result[match.group(1)] = match.group(2)
+except FileNotFoundError:
+    pass
+
+c = sys.argv[1].upper()
+v = sys.argv[2]
+result[f"CANISTER_ID_{c}"] = v
+
+with open(".env", 'w') as out:
+    for k, v in result.items():
+        out.write(f"{k}={shlex.quote(v)}\n")
